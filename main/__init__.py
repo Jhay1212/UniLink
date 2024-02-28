def665285e622eaefdbd2ed51170ec3241bbcbfc
Jhay Rivera <jhayjane78@gmail.com>
2024-02-28 18:35:14 +0800

    turns setup and configuration into function

--- main/__init__.py
+++ main/__init__.py
@@ -2,11 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask('__main__', template_folder='main/template')
-app.config['SECRET_KEY'] = '7a84ec5ac2294106cf137011fe57b60a5c74f9e6ba6e002b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy()
-db.init_app(app)
-app.app_context().push()
+def create_app():
+
+    app = Flask('__main__', template_folder='main/template')
+    app.config['SECRET_KEY'] = '7a84ec5ac2294106cf137011fe57b60a5c74f9e6ba6e002b'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    app.app_context().push()
+    return app
+
+app = create_app()
+
+def create_db():
+    db = SQLAlchemy()
+    db.init_app(app)
+    return db
+
+db = create_db()
 
 from main import routes
